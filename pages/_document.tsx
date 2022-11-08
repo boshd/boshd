55550d1ae9ab89f215f7f47706c62a84fe52edb8
Kareem Arab <me@kareemarab.com>
2022-11-07 20:24:23 -0500

    hello2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,10 +25,6 @@ export default class extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Nabla&display=swap" rel="stylesheet" />
-
-          <style>
-            @import url('https://fonts.googleapis.com/css2?family=Nabla&family=Newsreader:opsz@6..72&display=swap');
-          </style>
         </Head>
         <Main />
         <NextScript />
